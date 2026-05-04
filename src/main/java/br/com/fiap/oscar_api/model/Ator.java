898f2c0b1d5eafe1c2186d90e4a253f8bb9b0c4d
Laboratório FIAP <labsfiap@fiap.com.br>
2026-05-04 20:27:33 -0300

    finalizacao cp 02

--- src/main/java/br/com/fiap/oscar_api/model/Ator.java
+++ src/main/java/br/com/fiap/oscar_api/model/Ator.java
@@ -3,20 +3,28 @@ package br.com.fiap.oscar_api.model;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @Entity
 @NoArgsConstructor
+@Table (name = "atores")
 public class Ator {
 
     @Id
-    Long id;
+    private Long id;
 
     @Column (name = "nome_ator", length = 100, nullable = false)
-    String nome;
+    private String nome;
     
     @Column (nullable = false)
-    Integer numFilmes;
+    private Integer numFilmes;
+
+    @Column (length = 3)
+    private Integer idade;
+
+    @Column (length = 3, nullable = false)
+    private Integer numOscars;
 }
--- src/main/java/br/com/fiap/oscar_api/model/Filme.java
+++ src/main/java/br/com/fiap/oscar_api/model/Filme.java
@@ -3,22 +3,27 @@ package br.com.fiap.oscar_api.model;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @Entity
 @NoArgsConstructor
+@Table (name = "filmes")
 public class Filme {
     @Id
-    Long id;
+    private Long id;
 
     @Column (name = "nome_filme", length = 100, nullable = false)
-    String nome;
+    private String nome;
 
     @Column (length = 30, nullable = false)
-    Integer numPremiacoes;
+    private Integer numPremiacoes;
 
     @Column (length = 30, nullable = false)
-    Integer qtdCategoriasDisputadas;
+    private Integer qtdCategoriasDisputadas;
+
+    @Column(length = 100, nullable = false)
+    private String categoria;
 }
